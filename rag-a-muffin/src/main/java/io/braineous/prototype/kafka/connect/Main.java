5e414e6775293916d023fdb721e1970d078547b6
babyboy <braineous.engineering@gmail.com>
2025-09-03 21:58:07 -0400

    kafka_connect:phase_prototype

---
+++ rag-a-muffin/src/main/java/io/braineous/prototype/kafka/connect/Main.java
@@ -0,0 +1,9 @@
+package io.braineous.prototype.kafka.connect;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        System.out.println("hello_kafka_connect");
+    }
+
+}
