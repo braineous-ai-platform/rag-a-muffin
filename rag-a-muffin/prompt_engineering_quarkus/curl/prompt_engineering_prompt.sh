8b40133a76f3a0dd8ddaa1eefe8b5e3b503d317e
babyboy <braineous.engineering@gmail.com>
2025-10-09 17:20:43 -0500

    git_cleanup

--- rag-a-muffin/prompt_engineering_quarkus/curl/prompt_engineering_prompt.sh
+++ rag-a-muffin/prompt_engineering_quarkus/curl/prompt_engineering_prompt.sh
@@ -1,3 +1,3 @@
-curl -s -v -X POST http://127.0.0.1:8080/hello \
+curl -s -v -X POST http://127.0.0.1:8080/prompt \
   -H 'Content-Type: application/json' \
   -d '{"item":"Book","quantity":2,"price":10.5}'
