6840c5ef9b708c09166538ff22e31325d19e8f4f
babyboy <braineous.engineering@gmail.com>
2025-10-09 18:10:36 -0500

    prompt_engineering -> context_modeling

---
+++ rag-a-muffin/prompt_engineering_quarkus/src/test/java/ai/braineous/rag/prompt/PromptEngineeringTests.java
@@ -0,0 +1,13 @@
+package ai.braineous.rag.prompt;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PromptEngineeringTests {
+
+    @Test
+    public void testContextModel() throws Exception {
+        System.out.println("____testContextModel____");
+    }
+}
--- rag-a-muffin/prompt_engineering_quarkus/src/test/java/ai/braineous/rag/prompt/models/Context.java
+++ rag-a-muffin/prompt_engineering_quarkus/src/test/java/ai/braineous/rag/prompt/models/Context.java
@@ -1,5 +1,30 @@
 package ai.braineous.rag.prompt.models;
 
-public class Context {
+import java.io.Serializable;
 
+public class Context implements Serializable{
+
+        private String role;
+
+        private String rules;
+
+        private String facts;
+
+        private String content;
+
+        private String audience;
+
+        private String tone;
+
+        private String referenceData;
+
+        public Context(){
+
+        }
+
+        //-----------------------------------------------------------
+        //TODO: accessors, utilities, json generation once the model 
+        //is lifted upwards where necessary from String -> Object
+
+        //----------------------------------------------------------
 }
