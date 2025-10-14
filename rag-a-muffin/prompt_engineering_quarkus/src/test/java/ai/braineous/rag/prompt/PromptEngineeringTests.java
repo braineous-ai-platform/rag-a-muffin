05dad869c707b2a46a6c8bffb0fedab7abaa608a
babyboy <braineous.engineering@gmail.com>
2025-10-14 09:09:35 -0500

    prompt_engineering -> llm_prompt_orchestration -> LLMPromptOrchestrator -> reasoning -> mom_son_relationship -> meme

--- rag-a-muffin/prompt_engineering_quarkus/src/test/java/ai/braineous/rag/prompt/PromptEngineeringTests.java
+++ rag-a-muffin/prompt_engineering_quarkus/src/test/java/ai/braineous/rag/prompt/PromptEngineeringTests.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import ai.braineous.rag.prompt.utils.Resources;
 
 public class PromptEngineeringTests {
@@ -12,6 +15,9 @@ public class PromptEngineeringTests {
     public void testContextModel() throws Exception {
         System.out.println("____testContextModel____");
 
-        System.out.println(Resources.getResource("models/simple_query_relationship_dataset/weaviate_embeddings.json"));
+        String jsonStr = Resources.getResource("models/reasoning/mom_son_relationship/meme.json");
+        JsonObject json = JsonParser.parseString(jsonStr).getAsJsonObject();
+
+        System.out.println(json);
     }
 }
