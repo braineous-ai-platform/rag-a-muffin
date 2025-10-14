657dc0145026fca579403c677b6105dd7878c00e
babyboy <braineous.engineering@gmail.com>
2025-10-14 09:39:59 -0500

    prompt_engineering -> llm_prompt_orchestration -> LLMPromptOrchestrator -> reasoning -> mom_son_relationship -> meme

--- rag-a-muffin/prompt_engineering_quarkus/src/test/java/ai/braineous/rag/prompt/ReasoningIntegrationTests.java
+++ rag-a-muffin/prompt_engineering_quarkus/src/test/java/ai/braineous/rag/prompt/ReasoningIntegrationTests.java
@@ -9,11 +9,11 @@ import com.google.gson.JsonParser;
 
 import ai.braineous.rag.prompt.utils.Resources;
 
-public class PromptEngineeringTests {
+public class ReasoningIntegrationTests {
 
     @Test
-    public void testContextModel() throws Exception {
-        System.out.println("____testContextModel____");
+    public void testReasoningMemePathAndAnswer() throws Exception {
+        System.out.println("____reasoning_meme_path_and_answer____");
 
         String jsonStr = Resources.getResource("models/reasoning/mom_son_relationship/meme.json");
         JsonObject json = JsonParser.parseString(jsonStr).getAsJsonObject();
