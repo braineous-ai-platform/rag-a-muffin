c0139217f3b8b8020f39d471699cf7b6f9a560f0
babyboy <braineous.engineering@gmail.com>
2025-10-17 17:39:11 -0500

    cog -> reasoning -> orchestration -> implementation -> causal ->fact_extractor

--- rag-a-muffin/prompt_engineering_quarkus/src/main/java/ai/braineous/rag/prompt/services/cgo/causal/CausalFactExtractor.java
+++ rag-a-muffin/prompt_engineering_quarkus/src/main/java/ai/braineous/rag/prompt/services/cgo/causal/CausalFactExtractor.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
@@ -26,11 +27,15 @@ public class CausalFactExtractor implements FactExtractor{
 
             String id = prompt + ":" + i;
             String text = factJson.get("text").getAsString();
+            JsonObject featsJson = factJson.getAsJsonObject("feats");
 
             //get feats
+            Gson gson = new Gson();
+            featsMap = gson.fromJson(featsJson, Map.class);
 
             fact.setId(id);
             fact.setText(text);
+            fact.setFeats(featsMap);
 
             facts.add(fact);
         }
