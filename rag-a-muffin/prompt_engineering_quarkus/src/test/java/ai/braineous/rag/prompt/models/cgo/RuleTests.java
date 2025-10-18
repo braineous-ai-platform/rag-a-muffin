38631319c964405a2cba12d4c0348f6f0a275f19
babyboy <braineous.engineering@gmail.com>
2025-10-18 18:45:36 -0500

    cog -> reasoning -> orchestration -> implementation -> causal -> rule_engine

---
+++ rag-a-muffin/prompt_engineering_quarkus/src/test/java/ai/braineous/rag/prompt/models/cgo/RuleTests.java
@@ -0,0 +1,13 @@
+package ai.braineous.rag.prompt.models.cgo;
+
+import org.junit.jupiter.api.Test;
+
+import ai.braineous.rag.prompt.utils.Console;
+
+public class RuleTests {
+
+    @Test
+    public void testBuildRuleTopology() {
+        Console.log("test_rule_topology", null);
+    }
+}
