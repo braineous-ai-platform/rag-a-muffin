7305d57181a8af08d3e65b742deaa4ab604f3e7c
Sohil Shah - BabyBoy <braineous.engineering@gmail.com>
2025-12-08 18:09:56 -0600

    **feat: Add Scorer core with History integration**

--- rag-a-muffin/braineous/cgo-core/src/main/java/ai/braineous/rag/prompt/cgo/api/BusinessRule.java
+++ rag-a-muffin/braineous/cgo-core/src/main/java/ai/braineous/rag/prompt/cgo/api/BusinessRule.java
@@ -1,7 +1,5 @@
 package ai.braineous.rag.prompt.cgo.api;
 
-import ai.braineous.rag.prompt.models.cgo.graph.Proposal;
-
 @FunctionalInterface
 public interface BusinessRule {
     WorldMutation execute(GraphView view);
